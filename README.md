2b116be98e1da6f2a0b8e1c4dd9e0d1749acb301
caodaxing <2363278350cao@gmail.com>
2019-01-09 16:05:18 +0800

    新增http请求工具,接口文档更新

--- README.md
+++ README.md
@@ -1,1 +1,120 @@
 # seckill-shop
+### 1 oauth2授权
+&emsp;当外部调用应用对外的http接口时,需进行oauth2授权,通过调用授权接口获得access_token,在每次http请求中<br>
+带上access_token进行权限验证
+
+#### 1.1 授权接口
+
+**接口描述:** 获取认证token,用于访问资源URL进行权限认证,token有限时间默认 10 minutes;
+另外还会返回一个refresh_token,用于在token过期后进行更新<br>
+**请求方式:** POST<br>
+**接口地址:** http://localhost:8080/oauth/authorization<br>
+**接口参数:**
+
+参数名称|参数类型|是否必须|备注
+:--:|:--:|:--:|:--:
+client_id|String|是|应用ID
+client_secre|String|是|应用安全key
+grant_type|String|是|参数值:client_credentials,拼接于URL后面
+
+**java例子**
+```java
+public String getAccessToken(String client_id, String client_secret)throws Exception{
+	CloseableHttpClient httpClient = HttpClients.createDefault();
+    String newUrl = url + "?grant_type=client_credentials";
+    HttpPost post = new HttpPost(newUrl);
+    //增加请求配置
+	RequestConfig config = RequestConfig.custom()
+                    .setConnectTimeout(5000)
+                    .setConnectionRequestTimeout(2000)
+                    .setSocketTimeout(15000).build();
+	post.setConfig(config);
+    //拼接验证信息
+    String authInfo = clientId + ":" + clientSecret;
+    //进行base64编码
+    String encodeInfo = Base64.encodeBase64String(authInfo.getBytes());
+    // 进行授权
+    // 注意:Basic后面有一个空格,不然识别不了clientId和clientSecret,会报错
+    post.setHeader("Authorization", "Basic " + encodeInfo);
+    //设置参数
+    List<NameValuePair> entryList = new ArrayList<>();
+    Set<Entry<String, Object>> entrys = param.entrySet();
+    for (Entry<String, Object> entry : entrys) {
+        entryList.add(new BasicNameValuePair(entry.getKey(), entry.getValue().toString()));
+    }
+    post.setEntity(new UrlEncodedFormEntity(entryList, "utf-8"));
+    CloseableHttpResponse response = httpClient.execute(post);
+    String returnData = EntityUtils.toString(response.getEntity(),"utf-8");
+    return returnData;
+}
+```
+**返回参数**
+
+```json
+{
+	"access_token":"2400eebdb43a64a2507aa537a06af4ab",
+	"refresh_token":"7986417b3e209ff493396c5ec2836593"
+}
+```
+
+**错误**
+```json
+{
+	"error":"错误信息"
+}
+```
+---
+#### 1.2 刷新token接口
+**接口描述:** 当access_token过期后,通过refresh_token可以对access_token进行更新<br>
+**请求方式:** GET
+**接口地址:** http://localhost:8080/oauth/refreshToken<br>
+**接口参数:**
+
+参数名称|参数类型|是否必须|备注
+:--:|:--:|:--:|:--:
+client_id|String|是|应用ID
+refresh_token|String|是|刷新token
+
+**java例子**
+```java
+public String getAccessToken(String client_id, String refresh_token)throws Exception{
+	CloseableHttpClient httpClient = HttpClients.createDefault();
+    String newUrl = url + "?client_id="+client_id+"&refresh_token="+refresh_token;
+    HttpGet get = new HttpGet(newUrl);
+    //增加请求配置
+	RequestConfig config = RequestConfig.custom()
+                    .setConnectTimeout(5000)
+                    .setConnectionRequestTimeout(2000)
+                    .setSocketTimeout(15000).build();
+	get.setConfig(config);
+    CloseableHttpResponse response = httpClient.execute(get);
+    String returnData = EntityUtils.toString(response.getEntity(),"utf-8");
+    return returnData;
+}
+```
+**返回参数**
+
+```json
+{
+	"access_token":"2400eebdb43a64a2507aa537a06af4ab"
+}
+```
+
+**错误**
+```json
+{
+	"error":"错误信息"
+}
+```
+
+---
+### 2 seckill
+!!!
+#### 2.1 查询商品信息接口
+!!!
+#### 2.2 查询商品列表接口
+!!!
+#### 2.3 获取秒杀商品token接口
+!!!
+#### 2.4 秒杀商品接口
+!!!
--- src/main/java/com/caodaxing/shopseckill/utils/HttpRequestUtil.java
+++ src/main/java/com/caodaxing/shopseckill/utils/HttpRequestUtil.java
@@ -1,142 +1,211 @@
 package com.caodaxing.shopseckill.utils;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.lang.StringUtils;
-
+import com.alibaba.druid.util.StringUtils;
 import com.google.common.collect.Lists;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 public class HttpRequestUtil {
 
-	private static Builder builder;
+    private volatile static Builder builder;
+    private static CloseableHttpClient httpClient;
+    private static CloseableHttpResponse response;
 
-	public static void toPost(String url, Map<String, Object> params) {
-		//TODO
-	}
+    private static void initialize(HttpRequestBase request) throws UnsupportedEncodingException {
+        request.setConfig(getConfig());
+        if (builder == null) {
+            return;
+        }
+        if (builder.headers != null) {
+            request.setHeaders(builder.getHeaders());
+        }
+        if (request instanceof HttpPost) {
+            HttpPost post = (HttpPost) request;
+            if (builder.getEntities() != null) {
+                post.setEntity(new UrlEncodedFormEntity(builder.getEntities(), "utf-8"));
+            }
+        }
+    }
 
-	public static void toGet() {
-		//TODO
-	}
+    private static String jointUrl(String originalUrl){
+        if(builder == null || builder.getParameters() == null)
+            return originalUrl;
+        StringBuffer newUrl = new StringBuffer(originalUrl);
+        for (Parameter parameter : builder.getParameters()) {
+            if(newUrl.indexOf("?") <= 0){
+                newUrl.append("?").append(parameter.getName()).append("=").append(parameter.getValue());
+            }else {
+                newUrl.append("&").append(parameter.getName()).append(parameter.getValue());
+            }
+        }
+        return newUrl.toString();
+    }
 
-	public static Builder createBuilder() {
-		builder = new Builder();
-		return builder;
-	}
+    private static HttpEntity getResponse(HttpRequestBase request) throws IOException {
+        httpClient = HttpClients.createDefault();
+        response = httpClient.execute(request);
+        if(response != null){
+            return response.getEntity();
+        }
+        return null;
+    }
 
-	protected static class Builder {
+    private static RequestConfig getConfig() {
+        //增加请求配置
+        return RequestConfig.custom()
+                .setConnectTimeout(5000)
+                .setConnectionRequestTimeout(2000)
+                .setSocketTimeout(15000).build();
+    }
 
-		private List<Header> headers;
-		private List<Cookie> cookiese;
+    private static void clear() {
+        if(builder != null)
+            builder.clear();
+        try {
+            if (httpClient != null) {
+                httpClient.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-		public Builder setHeader(String key, String value) {
-			if (StringUtils.isBlank(key) || StringUtils.isBlank(value)) {
-				throw new RuntimeException("key or value is null!");
-			}
-			return this.setHeader(new Header(key, value));
-		}
+    static String toPost(String url) {
+        String result = null;
+        try {
+            HttpPost post = new HttpPost(jointUrl(url));
+            initialize(post);
+            HttpEntity entity = getResponse(post);
+            result = entity == null ? null : EntityUtils.toString(entity,"UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            clear();
+        }
+        return result;
+    }
 
-		public Builder setHeader(Header header) {
-			if (this.headers == null) {
-				this.headers = Lists.newArrayList();
-			}
-			this.headers.add(header);
-			return this;
-		}
+    public static String toGet(String url) {
+        String result = null;
+        try {
+            HttpGet get = new HttpGet(jointUrl(url));
+            initialize(get);
+            HttpEntity entity = getResponse(get);
+            result = entity == null ? null : EntityUtils.toString(entity,"UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            clear();
+        }
+        return result;
+    }
 
-		public Builder setCookie(String name, String value) {
-			if (StringUtils.isBlank(name) || StringUtils.isBlank(value)) {
-				throw new RuntimeException("key or value is null!");
-			}
-			return this.setCookie(new Cookie(name, value));
-		}
+    static Builder createBuilder() {
+        builder = new Builder();
+        return builder;
+    }
 
-		public Builder setCookie(Cookie cookie) {
-			if (this.cookiese == null) {
-				this.cookiese = Lists.newArrayList();
-			}
-			this.cookiese.add(cookie);
-			return this;
-		}
+    protected static class Builder {
 
-		public List<Header> getHeaders() {
-			return headers;
-		}
+        private List<BasicHeader> headers;
+        private List<Parameter> parameters;
+        private List<BasicNameValuePair> entities;
 
-		public void setHeaders(List<Header> headers) {
-			this.headers = headers;
-		}
+        private void clear() {
+            this.headers = null;
+            this.parameters = null;
+            this.entities = null;
+        }
 
-		public List<Cookie> getCookiese() {
-			return cookiese;
-		}
+        Builder setHeader(String key, String value) {
+            if (StringUtils.isEmpty(key) || StringUtils.isEmpty(value)) {
+                throw new RuntimeException("key or value is null!");
+            }
+            return this.setHeader(new BasicHeader(key, value));
+        }
 
-		public void setCookiese(List<Cookie> cookiese) {
-			this.cookiese = cookiese;
-		}
+        Builder setHeader(BasicHeader header) {
+            if (this.headers == null) {
+                this.headers = Lists.newArrayList();
+            }
+            this.headers.add(header);
+            return this;
+        }
 
-	}
+        public void setHeaders(List<BasicHeader> headers) {
+            this.headers = headers;
+        }
 
-	static class Cookie {
+        Builder setEntity(String name, String value) {
+            if (StringUtils.isEmpty(name) || StringUtils.isEmpty(value)) {
+                throw new RuntimeException("name or value is null!");
+            }
+            return this.setEntity(new BasicNameValuePair(name, value));
+        }
 
-		private String name;
-		private String value;
+        Builder setEntity(BasicNameValuePair entity) {
+            if (this.headers == null) {
+                this.headers = Lists.newArrayList();
+            }
+            this.entities.add(entity);
+            return this;
+        }
 
-		public Cookie() {
-		}
+        Builder setParameter(String key, String value) {
+            if (StringUtils.isEmpty(key) || StringUtils.isEmpty(value)) {
+                throw new RuntimeException("key or value is null!");
+            }
+            return this.setParameter(new Parameter(key, value));
+        }
 
-		public Cookie(String name, String value) {
-			this.name = name;
-			this.value = value;
-		}
+        Builder setParameter(Parameter parameter) {
+            if (this.parameters == null) {
+                this.parameters = Lists.newArrayList();
+            }
+            this.parameters.add(parameter);
+            return this;
+        }
 
-		public String getName() {
-			return name;
-		}
+        BasicHeader[] getHeaders() {
+            BasicHeader[] basicHeaders = new BasicHeader[this.headers.size()];
+            this.headers.toArray(basicHeaders);
+            return basicHeaders;
+        }
 
-		public void setName(String name) {
-			this.name = name;
-		}
+        List<BasicNameValuePair> getEntities() {
+            return this.entities;
+        }
 
-		public String getValue() {
-			return value;
-		}
+        List<Parameter> getParameters() {
+            return this.parameters;
+        }
 
-		public void setValue(String value) {
-			this.value = value;
-		}
+    }
 
-	}
-
-	static class Header {
-
-		private String key;
-		private String value;
-
-		public Header() {
-		}
-
-		public Header(String key, String value) {
-			this.key = key;
-			this.value = value;
-		}
-
-		public String getKey() {
-			return key;
-		}
-
-		public void setKey(String key) {
-			this.key = key;
-		}
-
-		public String getValue() {
-			return value;
-		}
-
-		public void setValue(String value) {
-			this.value = value;
-		}
-
-	}
-
+    static class Parameter extends BasicNameValuePair {
+        Parameter(String key, String value) {
+            super(key, value);
+        }
+    }
 }
